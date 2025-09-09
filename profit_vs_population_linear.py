90acbf88dbce281237b7a0395eca1a45934982c2
anishpdm <anish.vilayil.s@gmail.com>
2025-09-09 21:39:16 +0530

    Final Code

--- profit_vs_population_linear.py
+++ profit_vs_population_linear.py
@@ -1,16 +1,31 @@
 import pandas as pd
 import sklearn.linear_model as lm
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
 dataset= pd.read_csv("data.csv")
 x= dataset[["population"]]
 y= dataset[["profit"]]
-plt.scatter(x,y)
-plt.xlabel("population")
-plt.ylabel("profit")
-plt.show()
+
 model = lm.LinearRegression()
 model.fit(x,y)
+print("Intercept", model.intercept_)
+print("Coefficient", model.coef_)
 input=pd.DataFrame([[35000]], columns=["population"])
 result=model.predict( input  )
 print( round(result[0][0] , 2))
+
+
+plt.scatter(x,y)
+plt.plot(x,model.predict(x), color="red")
+plt.xlabel("population")
+plt.ylabel("profit")
+plt.show()
+
+y_pred= model.predict(x)
+#Evaluation
+print("MSE : ", mean_squared_error(y,y_pred))
+print("RMSE : ", np.sqrt(mean_squared_error(y,y_pred)))
+
+
